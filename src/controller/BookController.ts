bac642fe87c75f22c98df1eb87d3cfd0e6d0493d
Park <pbk95120@gmail.com>
2024-01-05 20:12:33 +0900

    feat: new books get api

--- src/controller/BookController.ts
+++ src/controller/BookController.ts
@@ -4,36 +4,40 @@ import conn from "../database/mariadb";
 import { RowDataPacket } from "mysql2";
 
 const allBooks = (req: Request, res: Response) => {
-  const { category_id } = req.params;
+  const { category_id, news } = req.query;
+  let sql = "SELECT * FROM books";
+  let values = [];
 
-  if (category_id) {
-    const sql = "SELECT * FROM books category_id = ?";
-    conn.query(sql, category_id, (err, results: RowDataPacket[]) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results);
-      }
-      return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    const sql = "SELECT * FROM books";
-    conn.query(sql, (err, results: RowDataPacket[]) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  if (category_id && news) {
+    sql +=
+      " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values = [category_id, news];
+  } else if (category_id) {
+    sql += " WHERE category_id=?";
+    values = [category_id];
+  } else if (news) {
+    sql +=
+      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values = [news];
   }
+
+  conn.query(sql, category_id, (err, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
 };
 
 const bookDetail = (req: Request, res: Response) => {
   const { id } = req.params;
-  const sql = "SELECT * FROM books WHERE id = ?";
+  const sql = `SELECT * FROM books LEFT JOIN categories ON books.category_id = categories.category_id WHERE books.id=?;`;
 
   conn.query(sql, id, (err, results: RowDataPacket[]) => {
     if (err) {
--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -4,7 +4,7 @@ import conn from "../database/mariadb";
 import { RowDataPacket } from "mysql2";
 
 const allCategories = (req: Request, res: Response) => {
-  const sql = "SELECT * FROM categories = ?";
+  const sql = "SELECT * FROM categories";
 
   conn.query(sql, (err, results: RowDataPacket[]) => {
     if (err) {
